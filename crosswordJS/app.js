599db44de98ec525834554f93f71de11afd72ace
Дмитрий <deslink123@gmail.com>
2023-04-14 14:49:03 +0300

    Styles adjustment

--- crosswordJS/app.js
+++ crosswordJS/app.js
@@ -1,8 +1,8 @@
 /** @type {HTMLDivElement} */
 const container = document.querySelector('#crosswordContainer')
 const columnsAmount = 6
-const rowsAmount = 9
-container.style.gridTemplateColumns = `repeat(${columnsAmount}, 60px)`
+const size = '50px'
+container.style.gridTemplateColumns = `repeat(${columnsAmount}, ${size})`
 
 const {words, letterBoxes} = getGrid()
 try {
@@ -14,10 +14,13 @@ try {
 function generateLayout() {
     letterBoxes.some(letterBox => {
             const div = document.createElement('div')
+            div.style.width = div.style.height = size
             div.className = 'letterBox'
             div.id = `LetterBox${letterBox.Id}`
             var text = ''
             if(letterBox.LetterIndex.length > 0){
+                div.style.backgroundColor = '#1144AA'
+                div.style.borderWidth = '2px'
                 var wordId = letterBox.WordId[0]
                 var letterIndex = letterBox.LetterIndex[0]
                 text = words.find(w => w.Id == wordId).Text[letterIndex].toUpperCase()
