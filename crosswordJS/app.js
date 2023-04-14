bb9cf51c2ba842be40ec357acdf0ed2334a3dafc
Дмитрий <deslink123@gmail.com>
2023-04-14 14:10:07 +0300

    First Crossword generated

--- crosswordJS/app.js
+++ crosswordJS/app.js
@@ -1,21 +1,28 @@
 /** @type {HTMLDivElement} */
 const container = document.querySelector('#crosswordContainer')
 const columnsAmount = 6
-container.style.gridTemplateColumns = `repeat(${columnsAmount}, 50px)`
-var counter = 0
+const rowsAmount = 9
+container.style.gridTemplateColumns = `repeat(${columnsAmount}, 60px)`
 
-generateLayout(6, 7)
+const {words, letterBoxes} = getGrid()
+try {
+    generateLayout()
+} catch (error) {
+    console.log(error);
+}
 
-
-function generateLayout(columnsAmount, rowsAmount) {
-    for (var i = 0; i < columnsAmount; i++) {
-        for (var j = 0; j < rowsAmount; j++) {
+function generateLayout() {
+    letterBoxes.some(letterBox => {
             const div = document.createElement('div')
             div.className = 'letterBox'
-            div.id = `${counter}`
-            div.textContent = div.id
+            div.id = `LetterBox${letterBox.Id}`
+            var text = ''
+            if(letterBox.LetterIndex.length > 0){
+                var wordId = letterBox.WordId[0]
+                var letterIndex = letterBox.LetterIndex[0]
+                text = words.find(w => w.Id == wordId).Text[letterIndex].toUpperCase()
+            }
+            div.textContent = `${text}`
             container.appendChild(div)
-            counter++
-        }
-    }
+    })
 }
---
+++ crosswordJS/model.js
@@ -0,0 +1,166 @@
+function getGrid() {
+    return {
+        words: getWords(),
+        letterBoxes: getLetterBoxes()
+    }
+}
+
+function getWords() {
+    return [
+        {
+            Id: 1,
+            Text: 'Сапоги',
+            Description: 'Вид обуви, предназначенный для влажной и холодной погоды.'
+        },
+        {
+            Id: 2,
+            Text: 'Топор',
+            Description: 'Инструмент для рубки дерева. Миниатюрная версия этого инструмента также использовалась как метательное оружие.'
+        },
+        {
+            Id: 3,
+            Text: 'Пергамент',
+            Description: 'Материал для письма из недублёной сыромятной кожи животных (до изобретения бумаги).'
+        },
+        {
+            Id: 4,
+            Text: 'Баран',
+            Description: 'Парнокопытное рогатое животное с густой шерстью. Часто используется как оскорбление в сторону мужчин.'
+        }]
+}
+
+function getLetterBoxes() {
+    var counter = 1;
+    var letterBoxes = [];
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= 6; j++, counter++) {
+            var id = counter
+            var wordId = []
+            var letterIndex = []
+            var row = i
+            var column = j
+            switch (i) {
+                case 1:
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(0)
+                    }
+                    break
+                case 2:
+                    if (j == 3) {
+                        wordId.push(2)
+                        letterIndex.push(0)
+                    }
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(1)
+                    }
+                    break
+                case 3:
+                    if (j == 3) {
+                        wordId.push(2)
+                        letterIndex.push(1)
+                    }
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(2)
+                    }
+                    break
+                case 4:
+                    if (j == 1) {
+                        wordId.push(1)
+                        letterIndex.push(0)
+                    }
+                    if (j == 2) {
+                        wordId.push(1)
+                        letterIndex.push(1)
+                    }
+                    if (j == 3) {
+                        wordId.push(1)
+                        letterIndex.push(2)
+                        wordId.push(2)
+                        letterIndex.push(2)
+                    }
+                    if (j == 4) {
+                        wordId.push(1)
+                        letterIndex.push(3)
+                    }
+                    if (j == 5) {
+                        wordId.push(1)
+                        letterIndex.push(4)
+                        wordId.push(3)
+                        letterIndex.push(3)
+                    }
+                    if (j == 6) {
+                        wordId.push(1)
+                        letterIndex.push(5)
+                    }
+                    break
+                case 5:
+                    if (j == 3) {
+                        wordId.push(2)
+                        letterIndex.push(3)
+                    }
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(4)
+                    }
+                    break
+                case 6:
+                    if (j == 1) {
+                        wordId.push(4)
+                        letterIndex.push(0)
+                    }
+                    if (j == 2) {
+                        wordId.push(4)
+                        letterIndex.push(1)
+                    }
+                    if (j == 3) {
+                        wordId.push(4)
+                        letterIndex.push(2)
+                        wordId.push(2)
+                        letterIndex.push(4)
+                    }
+                    if (j == 4) {
+                        wordId.push(4)
+                        letterIndex.push(3)
+                    }
+                    if (j == 5) {
+                        wordId.push(4)
+                        letterIndex.push(4)
+                        wordId.push(3)
+                        letterIndex.push(5)
+                    }
+                    break
+                case 7:
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(6)
+                    }
+                    break
+                case 8:
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(7)
+                    }
+                    break
+                case 9:
+                    if (j == 5) {
+                        wordId.push(3)
+                        letterIndex.push(8)
+                    }
+                    break
+            }
+
+            var letterBox = {
+                Id: id,
+                WordId: wordId,
+                LetterIndex: letterIndex,
+                Row: row,
+                Column: column
+            }
+            letterBoxes.push(letterBox)
+        }
+    }
+    return letterBoxes
+}
